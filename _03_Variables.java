3dcb12f5ddc6bb22e3bd1da856e196dac3ee4ab9
liangjf77 <89203828+liangjf77@users.noreply.github.com>
2022-12-31 21:31:47 +0900

    Add files via upload

---
+++ _03_Variables.java
@@ -0,0 +1,9 @@
+public class _03_Variables {
+    public static void main(String[] args) {
+        String name;
+        name = "我也开始编程了";
+
+        System.out.println(name + "님, 배송이 시작됩니. 15시에 방문 예정입니.");
+        System.out.println(name + "님, 배송이 완료되었습니다.");
+    }
+}
