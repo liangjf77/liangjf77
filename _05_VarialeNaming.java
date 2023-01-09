218e806817f88ac85e5396421fbbf0a652c9a2e4
liangjf77 <89203828+liangjf77@users.noreply.github.com>
2023-01-09 13:28:30 +0900

    Add files via upload

---
+++ _05_VarialeNaming.java
@@ -0,0 +1,21 @@
+package chap_01;
+
+public class _05_VarialbeNaming {
+    public static void main(String[] args) {
+        // 변수 이름 짓는 법
+        // 1. 자장할 값에 어울리는 이름
+        // 2. 맡줄(_), 문자(abc),숫자(133) 사용가능(공백 사용 불가)
+        // 3. 밑줄 또는 문자로 시작 가능
+        // 4. 한 단어 또는 2개 이상 단어의 연속
+        // 5. 소문자 시작, 각 단어의 시작 글자는 대문자( 첫 단어는 제외)
+        // 6. 예약어 사용 불가 (public, static, void, int. double, float...)
+
+        // 입국 신고서(여행)
+        String nationality = "USA";
+        String firstName = "Lily";
+        String LastName = "Laing";
+        String dateOfBirth = "2002-12-31";
+        String residentialAddress = "hotel";
+        String PurposeOfVisit = "travel";
+    }
+}
