7dde9c540db981e1cb2c159d522b1a7163bb3ea3
liangjf77 <89203828+liangjf77@users.noreply.github.com>
2023-01-10 12:29:28 +0900

    Add files via upload

---
+++ _06_Constants.java
@@ -0,0 +1,8 @@
+
+public class _06_Constants {
+    public static void main(String[] args) {
+        String CN_Country_CODE = "+86"; // 国家号码
+        CN_Country_CODE = "+8686";
+        System.out.println(CN_Country_CODE);
+    }
+}
